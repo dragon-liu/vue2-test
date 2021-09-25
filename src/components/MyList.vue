ba2b0168da43a836f79d64775615eafdce3e9a8b
dragon-liu <liuzhichao@hust.edu.cn>
2021-09-25 11:52:23 +0800

    Todolist静态组件编写

---
+++ src/components/MyList.vue
@@ -0,0 +1,47 @@
+<template>
+  <ul class="todo-main">
+    <MyItem></MyItem>
+    <MyItem></MyItem>
+    <MyItem></MyItem>
+    <MyItem></MyItem>
+  </ul>
+</template>
+
+<script>
+
+import MyItem from '@/components/MyItem'
+
+export default {
+  name:'MyList',
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  components: {
+    MyItem
+  }
+}
+</script>
+
+<style scoped>
+/*main*/
+  .todo-main {
+    margin-left: 0px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0px;
+  }
+  
+  .todo-empty {
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+  }
+</style>
